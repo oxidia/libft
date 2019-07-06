cba56c699d40ac8910a97eafbf09a2da2b816247
Youssef Bahlaoui <ybahlaou@e1r5p14.1337.ma>
2019-07-06 14:59:06 +0100

    fixing interger overflow

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ybahlaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/04 20:43:02 by ybahlaou          #+#    #+#             */
-/*   Updated: 2018/10/29 13:29:23 by ybahlaou         ###   ########.fr       */
+/*   Updated: 2019/07/06 14:58:03 by ybahlaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,20 @@
 int	ft_atoi(const char *str)
 {
 	unsigned int	nb;
+	unsigned int	max;
 	int				sign;
 
+	max = 1 << (sizeof(int) * 8 - 1);
 	while (ft_isspace(*str))
 		str++;
-	sign = 1;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
-		str++;
-	}
+	sign = *str == '-' ? -1 : 1;
+	str += *str == '-' || *str == '+';
 	nb = 0;
-	while (*str >= '0' && *str <= '9')
+	while (ft_isdigit(*str))
 	{
 		nb = 10 * nb + *str - '0';
+		if ((nb > max && sign == -1) || (sign == 1 && nb >= max))
+			return (-1);
 		str++;
 	}
 	return (nb * sign);
